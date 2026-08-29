1f92ef9fde34becdf4f5a088786485aff1c89541
seto <87982344+sertcozdemir@users.noreply.github.com>
2026-08-29 15:07:25 +0300

    post
    post

--- Assets/Settings/Scripts/Player.cs
+++ Assets/Settings/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd2619790b29d8d39b4d507f0622a06ec0307c87d7c2df0ed563b197b881a19c
-size 3314
+oid sha256:e38f533fee4ca0982486190942fff5e771007abc02bdc63d315cb7c5c3d8ba12
+size 3516
